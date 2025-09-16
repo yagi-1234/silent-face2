950921f84a051e4dd7264c5a3803431bc0b0825d
swwrb <swwrb674@yahoo.co.jp>
2025-09-17 01:05:31 +0900

    initial commit

---
+++ app/home/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import type { NextPage } from 'next'
+
+const Page: NextPage = () => {
+
+  return (
+    <div className="bg-white rounded-2xl w-240 p-6">
+      <h2 className="text-xl font-semibold mb-4">This is Home</h2>
+    </div>
+  )
+}
+
+export default Page
