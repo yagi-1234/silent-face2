e412a250dd025ebaaf80ef1ec94f76bf87da4e2a
swwrb <swwrb674@yahoo.co.jp>
2025-09-18 01:22:42 +0900

    20250918_2

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -25,7 +25,7 @@ const Page: NextPage = () => {
 
       <div className='flex items-center my-6'>
         <div className='flex-grow border-t border-gray-300'></div>
-        <span className='mx-4 text-gray-500'>Music</span>
+        <span className='mx-4 text-gray-500'>Musica</span>
         <div className='flex-grow border-t border-gray-300'></div>
       </div>
 
