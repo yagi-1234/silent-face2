735e1212ce324d33ddbef3766c2d478ad826a7df
swwrb <swwrb674@yahoo.co.jp>
2025-12-21 11:07:27 +0900

    20251221_2

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Suspense, useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import type { NextPage } from 'next'
 
 import { AlarmClockCheck, BookImage, BookOpenText, BookText, Calendar, Clapperboard, Disc3, Download, Gamepad2, Users, FileAudio, Music, MicVocal, SquareLibrary, Tv } from 'lucide-react'
@@ -22,39 +22,40 @@ const Page: NextPage = () => {
     <div className="root-panel">
       <h2 className="header-title">This is Home</h2>
 
-      <div className='flex items-center my-6'>
-        <div className='flex-grow border-t border-gray-300'></div>
-        <span className='mx-4 text-gray-500'>Task</span>
-        <div className='flex-grow border-t border-gray-300'></div>
+      <div className="flex items-center my-6">
+        <div className="flex-grow border-t border-gray-300"></div>
+        <span className="mx-4 text-gray-500">Task</span>
+        <div className="flex-grow border-t border-gray-300"></div>
       </div>
 
-      <div className='grid grid-cols-6 gap-10 text-center'>
+      <div className="grid grid-cols-3 sm:grid-cols-6 gap-10 text-center">
         <HomeIconButton icon={AlarmClockCheck} label="Task List" path="/tasks/tasks/taskList" />
         <HomeIconButton icon={AlarmClockCheck} label="Music Task" path="/tasks/music/musicTaskList" />
         <HomeIconButton icon={Calendar} label="Events" path="/tasks/events/eventList" />
       </div>
 
-      <div className='flex items-center my-6'>
-        <div className='flex-grow border-t border-gray-300'></div>
-        <span className='mx-4 text-gray-500'>Musica</span>
-        <div className='flex-grow border-t border-gray-300'></div>
+      <div className="flex items-center my-6">
+        <div className="flex-grow border-t border-gray-300"></div>
+        <span className="mx-4 text-gray-500">Music</span>
+        <div className="flex-grow border-t border-gray-300"></div>
       </div>
 
-      <div className='grid grid-cols-6 gap-10 text-center'>
-        <HomeIconButton icon={Users} label="Artists" path="/music/artists/artistList" />
-        <HomeIconButton icon={Disc3} label="Albums" path="/music/albums/albumList" />
-        <HomeIconButton icon={Music} label="Tracks" path="/music/tracks/trackList" />
-        <HomeIconButton icon={MicVocal} label="Lives" path="" />
-        <HomeIconButton icon={SquareLibrary} label="Playlists" path="/music/playlists/playlistsList" />
-        <HomeIconButton icon={Download} label="Track Import" path="/music/tracks/trackImport" />
+      <div className="grid grid-cols-3 sm:grid-cols-6 gap-10 text-center">
+          <HomeIconButton icon={Users} label="Artists" path="/music/artists/artistList" />
+          <HomeIconButton icon={Disc3} label="Albums" path="/music/albums/albumList" />
+          <HomeIconButton icon={Music} label="Tracks" path="/music/tracks/trackList" />
+          <HomeIconButton icon={MicVocal} label="Lives" path="" />
+          <HomeIconButton icon={SquareLibrary} label="Playlists" path="/music/playlists/playlistsList" />
+          <HomeIconButton icon={Download} label="Track Import" path="/music/tracks/trackImport" />
       </div>
 
-      <div className='flex items-center my-6'>
-        <div className='flex-grow border-t border-gray-300'></div>
-        <span className='mx-4 text-gray-500'>Libraries</span>
-        <div className='flex-grow border-t border-gray-300'></div>
+      <div className="flex items-center my-6">
+        <div className="flex-grow border-t border-gray-300"></div>
+        <span className="mx-4 text-gray-500">Libraries</span>
+        <div className="flex-grow border-t border-gray-300"></div>
       </div>
-      <div className='grid grid-cols-6 gap-10 text-center'>
+
+      <div className="grid grid-cols-3 sm:grid-cols-6 gap-10 text-center">
         <HomeIconButton icon={BookText} label="Books" path="/library/libraryList?library_type=02" />
         <HomeIconButton icon={BookImage} label="Comics" path="/library/libraryList?library_type=03" />
         <HomeIconButton icon={Clapperboard} label="Movies" path="/library/libraryList?library_type=04" />
