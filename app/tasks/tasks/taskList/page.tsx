49d67d2e714d31bdcc17d7856eb3bcfc36a422f1
swwrb <swwrb674@yahoo.co.jp>
2025-12-23 20:22:17 +0900

    20251223_1

--- app/tasks/tasks/taskList/page.tsx
+++ app/tasks/tasks/taskList/page.tsx
@@ -2,12 +2,11 @@
 
 import { Suspense, useEffect, useState } from 'react'
 import { useRouter, usePathname, useSearchParams } from 'next/navigation'
-import { ArrowLeft, Plus, FileText, CircleCheckBig, Info, Search } from 'lucide-react'
+import { AlarmClockCheck, ArrowLeft, BookImage, BookOpenText, BookText, CalendarClock, CircleCheckBig, Clapperboard, Hourglass, Plus, FileText, Gamepad2, Tv, Info, Search } from 'lucide-react'
 
 import { fetchTasks, updateLastActedAt, updateTaskStatus } from '@/actions/tasks/task-action'
 import { Breadcrumb } from '@/components/Breadcrumb'
 import ConfirmModal from '@/components/ConfirmModal'
-import { LogoffButton } from '@/components/LogoffButton'
 import MessageBanner from '@/components/MessageBanner'
 import HiddenPanel from '@/components/HiddenPanel'
 import { Tooltip, TooltipProvider, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip'
@@ -108,6 +107,24 @@ const TaskList = () => {
     return className
   }
 
+  const getTaskStatusClass = (taskStatus: string): string => {
+    if (taskStatus === '0') return "bg-gray-200"
+    if (taskStatus === '1') return "bg-red-200"
+    if (taskStatus === '2') return "bg-green-200"
+    if (taskStatus === '8') return "bg-yellow-100"
+    if (taskStatus === '9') return "bg-blue-200"
+    return ""
+  }
+
+  const getTaskTypeIcon = (taskType: string) => {
+    if (taskType === '02') return <BookText size={14} />
+    if (taskType === '03') return <BookImage size={14} />
+    if (taskType === '04') return <Clapperboard size={14} />
+    if (taskType === '05') return <Tv size={14} />
+    if (taskType === '06') return <Gamepad2 size={14} />
+    if (taskType === '07') return <BookOpenText size={14} />
+  }
+
   const loadList = async () => {
     const result = await fetchTasks(condition)
     setTasks(result)
@@ -159,124 +176,162 @@ const TaskList = () => {
           onClose={() => setMessage('')} />
       <div className="flex justify-between">
         <Breadcrumb />
-        <LogoffButton />
       </div>
       <h2 className="header-title">Task List</h2>
-      <div className="searchPanel">
-        <div className="input-form">
-          <label htmlFor="task_type">Type</label>
+      <div>
+        <div className="mb-2">
+          <label htmlFor="task_type" className="input-label">Task Type</label>
           <select
               id="task_type"
               name="task_type"
-              className="w-48"
+              className="w-40"
               value={condition.task_type}
               onChange={(e) => handleChange(e)}>
             <option key="" value=""></option>
             {Object.entries(CodeTaskType)
                 .sort(([a], [b]) => a.localeCompare(b))
-                .map(([key, label]) => (
-                  <option key={key} value={key}>
-                    {label}
-                  </option>
-            ))}
+                .map(([key, label]) => (<option key={key} value={key}>{label}</option>)
+            )}
           </select>
         </div>
-        <div className="input-form">
-          <label htmlFor="task_status">Task Status</label>
-          {Object.entries(CodeTaskStatus)
-              .sort(([a], [b]) => a.localeCompare(b))
-              .map(([key, label]) => (
-                <label key={key} className="input-check-label">
-                  <input type="checkbox"
-                      className=""
-                      value={key}
-                      checked={condition?.task_status_list.includes(key)}
-                      onChange={(e) => {
-                        setCondition(prev => ({
-                          ...prev,
-                          task_status_list: e.target.checked ? [...prev.task_status_list, key] : prev.task_status_list.filter(status => status !== key)
-                        }))
-                      }}
-                  />
-                  <span>{label}</span>
-                </label>
-          ))}
+        <div>
+          <label htmlFor="task_status" className="input-label">Task Status</label>
+          <div className="flex flex-wrap gap-x-4 gap-y-2">
+            {Object.entries(CodeTaskStatus)
+                .sort(([a], [b]) => a.localeCompare(b))
+                .map(([key, label]) => (
+                  <label key={key} className="input-check-label">
+                    <input type="checkbox"
+                        className=""
+                        value={key}
+                        checked={condition?.task_status_list.includes(key)}
+                        onChange={(e) => {
+                          setCondition(prev => ({
+                            ...prev,
+                            task_status_list: e.target.checked ? [...prev.task_status_list, key] : prev.task_status_list.filter(status => status !== key)
+                          }))
+                        }}
+                    />
+                    <span>{label}</span>
+                  </label>
+            ))}
+          </div>
+        </div>
+        <div className="div-row-right">
           <button className="button-search button-md"
               onClick={handleSearch}>
             <Search size={16} />
           </button>
         </div>
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Status</th>
-            <th>Type</th>
-            <th>Cycle</th>
-            <th>Name</th>
-            <th>Priority</th>
-            <th>Schedule Type</th>
-            <th>Progess</th>
-            <th>Action count</th>
-            <th>First Acted</th>
-            <th>Last Acted</th>
-            <th>Next Date</th>
-            <th>Limit Date</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task) => (
-            <tr key={task.task_id} className={getInputClassName(task.schedule_type, task.task_status, task.next_date, task.limit_date)}>
-              <td>
-                <select
-                    value={task.task_status}
-                    onChange={(e) => handleStatusChange(task.task_id, e.target.value) }>
-                  {Object.entries(CodeTaskStatus).map(([key, label]) => (
-                      <option key={key} value={key}>{label}</option>
-                  ))}
-                </select>
-              </td>
-              <td>{CodeTaskType[task.task_type] ?? ""}</td>
-              <td>{task.task_cycle}</td>
-              <td>{ellipsis(task.task_name, 24)}</td>
-              <td>{CodePriorityType[task.priority]}</td>
-              <td>{CodeScheduleType[task.schedule_type]}</td>
-              <td className="numeric-field">{task.task_progress}</td>
-              <td className="numeric-field">{task.action_count}</td>
-              <td>{formatDateTime(task.first_acted_at, "yyyy/MM/dd")}</td>
-              <td>{formatDateTime(task.last_acted_at, "yyyy/MM/dd")}</td>
-              <td>{formatDateTime(task.next_date, "yyyy/MM/dd")}</td>
-              <td>{formatDateTime(task.limit_date, "yyyy/MM/dd")}</td>
-              <td className="flex items-center gap-1">
-                <button className="bg-gray-100 text-green-700 flex items-center justify-center w-10 p-2"
-                    onClick={() => handleDoneAction(task.task_id)} >
-                  <CircleCheckBig className="w-5 h-5" />
-                </button>
-                <button
-                    className="button-page"
-                    onClick={() => handleShowForm(task.task_id)} >
-                  <FileText className="w-5 h-5" />
-                </button>
-                {task.task_comment && (
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger asChild>
-                        <button className="p-2 hover:bg-gray-100">
-                          <Info className="w-5 h-5" />
-                        </button>
-                      </TooltipTrigger>
-                      <TooltipContent side="top" className="bg-blue-500 text-white text-base px-2 py-1">
-                        <p>{task.task_comment}</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                )}
-              </td>
+      <div className="hidden sm:block">
+        <table>
+          <thead>
+            <tr>
+              <th>Status</th>
+              <th>Type</th>
+              <th>Cycle</th>
+              <th>Name</th>
+              <th>Priority</th>
+              <th>Schedule Type</th>
+              <th>Progess</th>
+              <th>Action count</th>
+              <th>First Acted</th>
+              <th>Last Acted</th>
+              <th>Next Date</th>
+              <th>Limit Date</th>
+              <th />
             </tr>
+          </thead>
+          <tbody>
+            {tasks.map(task => (
+              <tr key={task.task_id}
+                  className={getInputClassName(task.schedule_type, task.task_status, task.next_date, task.limit_date)}>
+                <td>
+                  <select
+                      value={task.task_status}
+                      onChange={(e) => handleStatusChange(task.task_id, e.target.value) }>
+                    {Object.entries(CodeTaskStatus).map(([key, label]) => (
+                        <option key={key} value={key}>{label}</option>
+                    ))}
+                  </select>
+                </td>
+                <td>{CodeTaskType[task.task_type] ?? ""}</td>
+                <td>{task.task_cycle}</td>
+                <td>{ellipsis(task.task_name, 24)}</td>
+                <td>{CodePriorityType[task.priority]}</td>
+                <td>{CodeScheduleType[task.schedule_type]}</td>
+                <td className="numeric-field">{task.task_progress}</td>
+                <td className="numeric-field">{task.action_count}</td>
+                <td>{formatDateTime(task.first_acted_at, "yyyy/MM/dd")}</td>
+                <td>{formatDateTime(task.last_acted_at, "yyyy/MM/dd")}</td>
+                <td>{formatDateTime(task.next_date, "yyyy/MM/dd")}</td>
+                <td>{formatDateTime(task.limit_date, "yyyy/MM/dd")}</td>
+                <td className="flex items-center gap-1">
+                  <button className="bg-gray-100 text-green-700 flex items-center justify-center w-10 p-2"
+                      onClick={() => handleDoneAction(task.task_id)} >
+                    <CircleCheckBig className="w-5 h-5" />
+                  </button>
+                  <button
+                      className="button-page"
+                      onClick={() => handleShowForm(task.task_id)} >
+                    <FileText className="w-5 h-5" />
+                  </button>
+                  {task.task_comment && (
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <button className="p-2 hover:bg-gray-100">
+                            <Info className="w-5 h-5" />
+                          </button>
+                        </TooltipTrigger>
+                        <TooltipContent side="top" className="bg-blue-500 text-white text-base px-2 py-1">
+                          <p>{task.task_comment}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="block sm:hidden">
+        <div className="div-card-area">
+          {tasks.map(task => (
+            <div key={task.task_id}
+                className="div-card">
+              <div className="div-card-row">
+                <label className={"card-label w-25 " + getTaskStatusClass(task.task_status)}>{CodeTaskStatus[task.task_status]}</label>
+                <div className="flex items-center card-label bg-blue-100 w-25">
+                  {getTaskTypeIcon(task.task_type)}
+                  <label>{CodeTaskType[task.task_type]}</label>
+                </div>
+                <label className="bg-purple-100 card-label">{task.task_cycle}</label>
+              </div>
+              <div>
+                <button
+                    className="button-link card-title"
+                    onClick={() => handleShowForm(task.task_id)}>
+                  {task.task_name}
+                </button>
+              </div>
+              <div className="div-card-row">
+                <AlarmClockCheck size={14} />
+                {formatDateTime(task.last_acted_at, "yyyy/MM/dd")}
+              </div>
+              <div className="div-card-row">
+                <CalendarClock size={14} />
+                {formatDateTime(task.next_date, "yyyy/MM/dd")}
+                <span>&ensp;</span>
+                <Hourglass size={14} />
+                {formatDateTime(task.limit_date, "yyyy/MM/dd")}
+              </div>
+            </div>
           ))}
-        </tbody>
-      </table>
+        </div>
+      </div>
       <div className="footer-area">
         <div className="footer-area-sub">
           <div className="footer-left">
@@ -303,6 +358,6 @@ const TaskList = () => {
           </>
         }
       />
-    </div>
+     </div>
   )
 }
